5af1efd951a68061f2a8075a549c6a73d889bddd
delesslin <delesslingw@gmail.com>
2020-11-21 18:11:33 -0500

    :rocket: initial electron prototype

---
+++ electron/index.js
@@ -0,0 +1,39 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const isDev = require('electron-is-dev')
+
+let mainWindow
+
+function createWindow() {
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+  mainWindow = new BrowserWindow({ width, height, kiosk: true })
+  // I think we probably just want the localhost option
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
+  if (isDev) {
+    // Open the DevTools.
+    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools()
+  }
+  mainWindow.on('closed', () => (mainWindow = null))
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
